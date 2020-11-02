9965dcb9196d622d92f2329d7bc9a796222a2795
Shamel Lakin <christlife4u@gmail.com>
2020-11-02 11:38:46 -0500

    fetch working

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const endPoint = 'http://localhost:3000/api/v1/quotes'
 
 document.addEventListener('DOMContentLoaded', () => {
-   fetch(endPoint)
-   .then()
+   getQuotes()
 })
+
+function getQuotes() {
+   fetch(endPoint)
+   .then(response => response.json())
+   .then(quotes_resp => {
+      quotes_resp.data.forEach(quotz => {
+         const quoteMark = `
+         <div data-id=${quotz.id}>
+         <h2>${quotz.attributes.quote}</h2>
+         <h3>${quotz.attributes.author}</h3>
+         <p>${quotz.attributes.category.name}</p>
+         <p>${quotz.attributes.tag}</p>
+         <button data-id=${quotz.id}>edit</button>
+         </div>`;
+
+         document.querySelector('#quotz-container').innerHTML += quoteMark 
+      })
+   })
+}
